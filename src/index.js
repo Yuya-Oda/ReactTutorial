842cd28958656a45d2d31eec1614b5a11cbac4b5
Yuya-Oda <yo.jobhunting@gmail.com>
2021-01-02 15:50:47 +0900

    チュートリアル終了

--- src/index.js
+++ src/index.js
@@ -1,29 +1,99 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-class Square extends React.Component {
-    render() {
+
+  // クラスコンポーネントは画面のロード時に一番最初に動かかすことができる
+  // class Square extends React.Component {
+  //   render() {
+  //     return (
+  //       <button
+  //         className="square"
+  //         onClick={() => this.props.onClick()}
+  //       >
+  //         {this.props.value}
+  //       </button>
+  //     );
+  //   }
+  // }
+ 
+  // ファンクションはそのままでは一番最初に動かせない。stateも使えない。
+  // しかし、フック(hook)を使用することで
+  // クラスコンポーネントの長所を使うことができるように
+  // なったため短所が無くなった。
+  // function Square(props) {
+  //   return (
+  //     <button className="square" onClick={props.onClick}>
+  //       {props.value}
+  //     </button>
+  //   );
+  // }
+
+  const Square = (props) => {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+
+  class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+        xIsNext: true,
+      };
+    }
+    
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      
+      // early return
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+      // 上の参考演算子は下のIF文と同義
+      // if(this.state.xIsNext === true){
+      //   squares[i] = 'X';
+      // }else{
+      //   squares[i] = 'O'
+      // }
+
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
+    }
+
+    renderSquare(i) {
       return (
-        <button className="square">
-          {/* TODO */}
-        </button>
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
       );
     }
-  }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
-      return <Square />;
-    }
   
     render() {
-      const status = 'Next player: X';
-  
+      // const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      
+      const winner = calculateWinner(this.state.squares);
+      let status;
+
+      if (winner　!= null) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+
       return (
         <div>
           <div className="status">{status}</div>
           <div className="board-row">
-            {this.renderSquare(0)}
+            {this.renderSquare(0)} 
             {this.renderSquare(1)}
             {this.renderSquare(2)}
           </div>
@@ -58,6 +128,25 @@ class Square extends React.Component {
     }
   }
   
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   // ========================================
   
   ReactDOM.render(
